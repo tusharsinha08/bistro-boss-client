b2eb8e417d81e9cc8c626c31bf5f02c21315b568
tusharsinha08 <121822994+tusharsinha08@users.noreply.github.com>
2025-05-29 04:34:24 +0600

    add menu, logo

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -4,6 +4,7 @@ import {
 } from "react-router-dom";
 import Main from "../Layout/Main";
 import Home from "../pages/Home/Home/Home";
+import Menu from "../pages/Menu/Menu";
 
 
 
@@ -15,6 +16,10 @@ export const router = createBrowserRouter([
             {
                 path: '/',
                 element: <Home></Home>
+            },
+            {
+                path: 'menu',
+                element: <Menu></Menu>
             }
         ]
     },
--- src/pages/Shared/Navbar/Navbar.jsx
+++ src/pages/Shared/Navbar/Navbar.jsx
@@ -1,20 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
 
     const navOptions = <>
-        <li><a>Item 1</a></li>
-        <li tabIndex={0} className='dropdown dropdown-bottom'>
-            <a className='justify-between'>
-                Parent
-                <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" /></svg>
-            </a>
-            <ul className="p-2 dropdown-content menu shadow bg-base-100 rounded-box w-52">
-                <li><a>Submenu 1</a></li>
-                <li><a>Submenu 2</a></li>
-            </ul>
-        </li>
-        <li><a>Item 3</a></li>
+        <li><Link to='/'>Home</Link></li>
+        <li><Link to="/menu">Our Menu</Link></li>
     </>
 
     return (
