0b7a47f4ca9b9a7f19f1a17381a5a1e37b34e64d
tusharsinha08 <121822994+tusharsinha08@users.noreply.github.com>
2025-05-29 04:55:32 +0600

    i react helmet async

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import { HelmetProvider } from 'react-helmet-async';
+
 
 import {
   RouterProvider,
@@ -11,8 +12,10 @@ import { router } from './Routes/Routes.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <div className='max-w-screen-xl mx-auto'>
-      <RouterProvider router={router} />
-    </div>
+    <HelmetProvider>
+      <div className='max-w-screen-xl mx-auto'>
+        <RouterProvider router={router} />
+      </div>
+    </HelmetProvider>
   </StrictMode>,
 )
--- src/pages/Menu/Menu.jsx
+++ src/pages/Menu/Menu.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import { Helmet } from 'react-helmet-async';
 
 const Menu = () => {
     return (
         <div>
+            <Helmet>
+                <title>Bistro Boss | Menu</title>
+            </Helmet>
             <h2>this is menu</h2>
         </div>
     );
