b559d7014a7011a515b2177397025e35413fd60c
tusharsinha08 <121822994+tusharsinha08@users.noreply.github.com>
2025-08-09 07:53:52 +0600

    confirm payment method

--- src/pages/Dashboard/Payment/CheckOutForm.jsx
+++ src/pages/Dashboard/Payment/CheckOutForm.jsx
@@ -1,9 +1,28 @@
 import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
+import { useEffect, useState } from 'react';
+import useAxiosSecure from '../../../hooks/useAxiosSecure';
+import useCart from '../../../hooks/useCart';
+import useAuth from '../../../hooks/useAuth';
 
 const CheckOutForm = () => {
 
     const stripe = useStripe();
     const elements = useElements();
+    const [error, setError] = useState('')
+    const [transactionId, setTransactionId] = useState('')
+    const [clientSecret, setClientSecret] = useState('')
+    const axiosSecure = useAxiosSecure()
+    const [cart] = useCart()
+    const { user } = useAuth()
+    const totalPrice = cart.reduce((total, item) => total + item.price, 0)
+
+    useEffect(() => {
+        axiosSecure.post('/create-payment-intent', { price: totalPrice })
+            .then(res => {
+                // console.log(res.data.clientSecret);
+                setClientSecret(res.data.clientSecret)
+            })
+    })
 
     const handleSubmit = async event => {
         event.preventDefault()
@@ -25,34 +44,66 @@ const CheckOutForm = () => {
 
         if (error) {
             console.log("error", error)
+            setError(error.message)
         }
         else {
             console.log("payment Method", paymentMethod)
+            setError('')
+        }
 
+        // confirm payment -------------
+        const { paymentIntent, error: confirmError } = await stripe.confirmCardPayment(clientSecret, {
+            payment_method: {
+                card: card,
+                billing_details: {
+                    email: user?.email || 'anonymous',
+                    name: user?.displayName || 'anonymous'
+                }
+            }
+            
+        })
+
+        if(confirmError){
+            console.log("confirm error: ", confirmError);
+        }
+        else {
+            console.log('payment intent: ', paymentIntent)
+            if(paymentIntent.status === 'succeeded') {
+                setTransactionId(paymentIntent.id)
+            }
         }
     }
 
     return (
         <form className='p-4' onSubmit={handleSubmit}>
-                <CardElement className='mb-4'
-                    options={{
-                        style: {
-                            base: {
-                                fontSize: '16px',
-                                color: '#424770',
-                                '::placeholder': {
-                                    color: '#aab7c4',
-                                },
-                            },
-                            invalid: {
-                                color: '#9e2146',
+            <CardElement className='mb-4'
+                options={{
+                    style: {
+                        base: {
+                            fontSize: '16px',
+                            color: '#424770',
+                            '::placeholder': {
+                                color: '#aab7c4',
                             },
                         },
-                    }}
-                />
-                <button className='p-2 px-3 bg-primary text-white rounded-md font-semibold' type="submit" disabled={!stripe}>
-                    Pay
-                </button>
+                        invalid: {
+                            color: '#9e2146',
+                        },
+                    },
+                }}
+            />
+            <button
+                className="p-2 px-3 btn btn-primary text-white rounded-md font-semibold"
+                type="submit"
+                disabled={!stripe || !clientSecret}>
+                Pay
+            </button>
+            <p>
+                <span className='text-error m-2'>{error}</span>
+            </p>
+            {
+                transactionId ? <p>Transaction Id:<span className='text-green-400'> {transactionId}</span></p> : ""
+            }
         </form>
     );
 };
---
+++ src/pages/Dashboard/Payment/__payment_steps_.js
@@ -0,0 +1,11 @@
+/**
+ * 1. install stripe and stripe react
+ * 2. create card element
+ * 3. create stripe account and get publishable key
+ * 4. use pk and use stripe to get card information and errors
+ * 5. create payment intent post on the server and return client secret. install stripe on server side and get client secret. make sure you used card payment method type: ['card']
+ * 6. from client side get the client secret and save it 
+ * 7. use confirm card payment and pass pass user information, card and client secret.
+ * 8. display transaction id
+ * 
+ * * */
